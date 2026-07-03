2342b890bb6df40da06c5bb38eaec144b81eaa68
bhanu2314578-tech <bhanu2314578@gmail.com>
2026-07-03 17:39:32 +0530

    logic for handrails2

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL||'http://127.0.0.1:8000'
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL||'https://costestimatorbackend-cdckbnh5gkdsfmgr.centralindia-01.azurewebsites.net'
 
 export const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
